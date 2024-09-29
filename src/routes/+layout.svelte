820dcde2fdf36b67f231708f26b3920fba759e96
LinhBQ <linhstartwork@gmail.com>
2024-09-29 09:18:06 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -354,12 +354,214 @@
             <span style="color: red;">{phoneError}</span>
           {/if}
         </td>
-        <td><input type="text" placeholder="Ngày sinh" bind:value={dob}></td>
+        <td><Flatpickr class='date-picker' bind:value={dob} placeholder='Ngày sinh' /></td>
   
       </tr>
       <tr>
         <td><input type="email" placeholder="Email" bind:value={email}></td>
-        <td><input type="text" placeholder="Quốc tịch" bind:value={nation}></td>
+        <td>
+          <select id="country" name="country" bind:value={nation} class='date-picker'>
+            <option value="Vietnam">Việt Nam</option>
+            <option value="Korea">Korea</option>
+            <option value="Afghanistan">Afghanistan</option>
+            <option value="Albania">Albania</option>
+            <option value="Algeria">Algeria</option>
+            <option value="Andorra">Andorra</option>
+            <option value="Angola">Angola</option>
+            <option value="Antigua and Barbuda">Antigua and Barbuda</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Armenia">Armenia</option>
+            <option value="Australia">Australia</option>
+            <option value="Austria">Austria</option>
+            <option value="Azerbaijan">Azerbaijan</option>
+            <option value="Bahamas">Bahamas</option>
+            <option value="Bahrain">Bahrain</option>
+            <option value="Bangladesh">Bangladesh</option>
+            <option value="Barbados">Barbados</option>
+            <option value="Belarus">Belarus</option>
+            <option value="Belgium">Belgium</option>
+            <option value="Belize">Belize</option>
+            <option value="Benin">Benin</option>
+            <option value="Bhutan">Bhutan</option>
+            <option value="Bolivia">Bolivia</option>
+            <option value="Bosnia and Herzegovina">Bosnia and Herzegovina</option>
+            <option value="Botswana">Botswana</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Brunei">Brunei</option>
+            <option value="Bulgaria">Bulgaria</option>
+            <option value="Burkina Faso">Burkina Faso</option>
+            <option value="Burundi">Burundi</option>
+            <option value="Côte d'Ivoire">Côte d'Ivoire</option>
+            <option value="Cabo Verde">Cabo Verde</option>
+            <option value="Cambodia">Cambodia</option>
+            <option value="Cameroon">Cameroon</option>
+            <option value="Canada">Canada</option>
+            <option value="Central African Republic">Central African Republic</option>
+            <option value="Chad">Chad</option>
+            <option value="Chile">Chile</option>
+            <option value="China">China</option>
+            <option value="Colombia">Colombia</option>
+            <option value="Comoros">Comoros</option>
+            <option value="Congo (Congo-Brazzaville)">Congo (Congo-Brazzaville)</option>
+            <option value="Costa Rica">Costa Rica</option>
+            <option value="Croatia">Croatia</option>
+            <option value="Cuba">Cuba</option>
+            <option value="Cyprus">Cyprus</option>
+            <option value="Czechia (Czech Republic)">Czechia (Czech Republic)</option>
+            <option value="Democratic Republic of the Congo">Democratic Republic of the Congo</option>
+            <option value="Denmark">Denmark</option>
+            <option value="Djibouti">Djibouti</option>
+            <option value="Dominica">Dominica</option>
+            <option value="Dominican Republic">Dominican Republic</option>
+            <option value="Ecuador">Ecuador</option>
+            <option value="Egypt">Egypt</option>
+            <option value="El Salvador">El Salvador</option>
+            <option value="Equatorial Guinea">Equatorial Guinea</option>
+            <option value="Eritrea">Eritrea</option>
+            <option value="Estonia">Estonia</option>
+            <option value="Eswatini (fmr. Swaziland)">Eswatini (fmr. Swaziland)</option>
+            <option value="Ethiopia">Ethiopia</option>
+            <option value="Fiji">Fiji</option>
+            <option value="Finland">Finland</option>
+            <option value="France">France</option>
+            <option value="Gabon">Gabon</option>
+            <option value="Gambia">Gambia</option>
+            <option value="Georgia">Georgia</option>
+            <option value="Germany">Germany</option>
+            <option value="Ghana">Ghana</option>
+            <option value="Greece">Greece</option>
+            <option value="Grenada">Grenada</option>
+            <option value="Guatemala">Guatemala</option>
+            <option value="Guinea">Guinea</option>
+            <option value="Guinea-Bissau">Guinea-Bissau</option>
+            <option value="Guyana">Guyana</option>
+            <option value="Haiti">Haiti</option>
+            <option value="Holy See">Holy See</option>
+            <option value="Honduras">Honduras</option>
+            <option value="Hungary">Hungary</option>
+            <option value="Iceland">Iceland</option>
+            <option value="India">India</option>
+            <option value="Indonesia">Indonesia</option>
+            <option value="Iran">Iran</option>
+            <option value="Iraq">Iraq</option>
+            <option value="Ireland">Ireland</option>
+            <option value="Israel">Israel</option>
+            <option value="Italy">Italy</option>
+            <option value="Jamaica">Jamaica</option>
+            <option value="Japan">Japan</option>
+            <option value="Jordan">Jordan</option>
+            <option value="Kazakhstan">Kazakhstan</option>
+            <option value="Kenya">Kenya</option>
+            <option value="Kiribati">Kiribati</option>
+            <option value="Kuwait">Kuwait</option>
+            <option value="Kyrgyzstan">Kyrgyzstan</option>
+            <option value="Laos">Laos</option>
+            <option value="Latvia">Latvia</option>
+            <option value="Lebanon">Lebanon</option>
+            <option value="Lesotho">Lesotho</option>
+            <option value="Liberia">Liberia</option>
+            <option value="Libya">Libya</option>
+            <option value="Liechtenstein">Liechtenstein</option>
+            <option value="Lithuania">Lithuania</option>
+            <option value="Luxembourg">Luxembourg</option>
+            <option value="Madagascar">Madagascar</option>
+            <option value="Malawi">Malawi</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="Maldives">Maldives</option>
+            <option value="Mali">Mali</option>
+            <option value="Malta">Malta</option>
+            <option value="Marshall Islands">Marshall Islands</option>
+            <option value="Mauritania">Mauritania</option>
+            <option value="Mauritius">Mauritius</option>
+            <option value="Mexico">Mexico</option>
+            <option value="Micronesia">Micronesia</option>
+            <option value="Moldova">Moldova</option>
+            <option value="Monaco">Monaco</option>
+            <option value="Mongolia">Mongolia</option>
+            <option value="Montenegro">Montenegro</option>
+            <option value="Morocco">Morocco</option>
+            <option value="Mozambique">Mozambique</option>
+            <option value="Myanmar (formerly Burma)">Myanmar (formerly Burma)</option>
+            <option value="Namibia">Namibia</option>
+            <option value="Nauru">Nauru</option>
+            <option value="Nepal">Nepal</option>
+            <option value="Netherlands">Netherlands</option>
+            <option value="New Zealand">New Zealand</option>
+            <option value="Nicaragua">Nicaragua</option>
+            <option value="Niger">Niger</option>
+            <option value="Nigeria">Nigeria</option>
+            <option value="North Korea">North Korea</option>
+            <option value="North Macedonia">North Macedonia</option>
+            <option value="Norway">Norway</option>
+            <option value="Oman">Oman</option>
+            <option value="Pakistan">Pakistan</option>
+            <option value="Palau">Palau</option>
+            <option value="Palestine State">Palestine State</option>
+            <option value="Panama">Panama</option>
+            <option value="Papua New Guinea">Papua New Guinea</option>
+            <option value="Paraguay">Paraguay</option>
+            <option value="Peru">Peru</option>
+            <option value="Philippines">Philippines</option>
+            <option value="Poland">Poland</option>
+            <option value="Portugal">Portugal</option>
+            <option value="Qatar">Qatar</option>
+            <option value="Romania">Romania</option>
+            <option value="Russia">Russia</option>
+            <option value="Rwanda">Rwanda</option>
+            <option value="Saint Kitts and Nevis">Saint Kitts and Nevis</option>
+            <option value="Saint Lucia">Saint Lucia</option>
+            <option value="Saint Vincent and the Grenadines">Saint Vincent and the Grenadines</option>
+            <option value="Samoa">Samoa</option>
+            <option value="San Marino">San Marino</option>
+            <option value="Sao Tome and Principe">Sao Tome and Principe</option>
+            <option value="Saudi Arabia">Saudi Arabia</option>
+            <option value="Senegal">Senegal</option>
+            <option value="Serbia">Serbia</option>
+            <option value="Seychelles">Seychelles</option>
+            <option value="Sierra Leone">Sierra Leone</option>
+            <option value="Singapore">Singapore</option>
+            <option value="Slovakia">Slovakia</option>
+            <option value="Slovenia">Slovenia</option>
+            <option value="Solomon Islands">Solomon Islands</option>
+            <option value="Somalia">Somalia</option>
+            <option value="South Africa">South Africa</option>
+            
+            <option value="South Sudan">South Sudan</option>
+            <option value="Spain">Spain</option>
+            <option value="Sri Lanka">Sri Lanka</option>
+            <option value="Sudan">Sudan</option>
+            <option value="Suriname">Suriname</option>
+            <option value="Sweden">Sweden</option>
+            <option value="Switzerland">Switzerland</option>
+            <option value="Syria">Syria</option>
+            <option value="Tajikistan">Tajikistan</option>
+            <option value="Tanzania">Tanzania</option>
+            <option value="Thailand">Thailand</option>
+            <option value="Timor-Leste">Timor-Leste</option>
+            <option value="Togo">Togo</option>
+            <option value="Tonga">Tonga</option>
+            <option value="Trinidad and Tobago">Trinidad and Tobago</option>
+            <option value="Tunisia">Tunisia</option>
+            <option value="Turkey">Turkey</option>
+            <option value="Turkmenistan">Turkmenistan</option>
+            <option value="Tuvalu">Tuvalu</option>
+            <option value="Uganda">Uganda</option>
+            <option value="Ukraine">Ukraine</option>
+            <option value="United Arab Emirates">United Arab Emirates</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="United States of America">United States of America</option>
+            <option value="Uruguay">Uruguay</option>
+            <option value="Uzbekistan">Uzbekistan</option>
+            <option value="Vanuatu">Vanuatu</option>
+            <option value="Venezuela">Venezuela</option>
+            
+            <option value="Yemen">Yemen</option>
+            <option value="Zambia">Zambia</option>
+            <option value="Zimbabwe">Zimbabwe</option>
+        </select>
+        
+          <!-- <input type="text" placeholder="Quốc tịch" bind:value={nation}> -->
+        </td>
       </tr>
       <tr>
         <td colspan="2"><input type="text" placeholder="Địa chỉ" bind:value={address}></td>
@@ -381,11 +583,61 @@
       </tr>
       <tr>
         <td>
-          <input type="date" id="appointment-date" placeholder="Ngày khám"  bind:value={date}>
+          <Flatpickr bind:value={date} placeholder='Ngày khám' class='date-picker'/>
+          <!-- <Datepicker bind:selected={date} /> -->
         </td>
         <td>
-          <select id="appointment-time"  bind:value={time}>
-            <option value="" disabled selected>Chọn Thời gian</option>
+          
+          <select id="appointment-time"  bind:value={time} placeholder='thoi gian'>
+            <option value="08:00">08:00 AM</option>
+            <option value="08:15">08:15 AM</option>
+            <option value="08:30">08:30 AM</option>
+            <option value="08:45">08:45 AM</option>
+            <option value="09:00">09:00 AM</option>
+            <option value="09:15">09:15 AM</option>
+            <option value="09:30">09:30 AM</option>
+            <option value="09:45">09:45 AM</option>
+            <option value="10:00">10:00 AM</option>
+            <option value="10:15">10:15 AM</option>
+            <option value="10:30">10:30 AM</option>
+            <option value="10:45">10:45 AM</option>
+            <option value="11:00">11:00 AM</option>
+            <option value="11:15">11:15 AM</option>
+            <option value="11:30">11:30 AM</option>
+            <option value="11:45">11:45 AM</option>
+            <option value="12:00">12:00 PM</option>
+            <option value="12:15">12:15 PM</option>
+            <option value="12:30">12:30 PM</option>
+            <option value="12:45">12:45 PM</option>
+            <option value="13:00">01:00 PM</option>
+            <option value="13:15">01:15 PM</option>
+            <option value="13:30">01:30 PM</option>
+            <option value="13:45">01:45 PM</option>
+            <option value="14:00">02:00 PM</option>
+            <option value="14:15">02:15 PM</option>
+            <option value="14:30">02:30 PM</option>
+            <option value="14:45">02:45 PM</option>
+            <option value="15:00">03:00 PM</option>
+            <option value="15:15">03:15 PM</option>
+            <option value="15:30">03:30 PM</option>
+            <option value="15:45">03:45 PM</option>
+            <option value="16:00">04:00 PM</option>
+            <option value="16:15">04:15 PM</option>
+            <option value="16:30">04:30 PM</option>
+            <option value="16:45">04:45 PM</option>
+            <option value="17:00">05:00 PM</option>
+            <option value="17:15">05:15 PM</option>
+            <option value="17:30">05:30 PM</option>
+            <option value="17:45">05:45 PM</option>
+            <option value="18:00">06:00 PM</option>
+            <option value="18:15">06:15 PM</option>
+            <option value="18:30">06:30 PM</option>
+            <option value="18:45">06:45 PM</option>
+            <option value="19:00">07:00 PM</option>
+            <option value="19:15">07:15 PM</option>
+            <option value="19:30">07:30 PM</option>
+            <option value="19:45">07:45 PM</option>
+            <option value="20:00">08:00 PM</option>
           </select>
         </td>
       </tr>
@@ -418,17 +670,22 @@
 </footer>
 
 <script>
+  import Flatpickr from 'svelte-flatpickr';
+
+  import 'flatpickr/dist/flatpickr.css'
+  import './custom.css';
+  // import DatePicker from 'svelte-date-picker'
   import { fade } from 'svelte/transition';
   let name = '';
   let gender = 'male';
   let phone = '';
   let dob = '';
   let email = '';
-  let nation = '';
+  let nation = 'Vietnam';
   let address = '';
   let depart = '';
   let date = '';
-  let time = '';
+  let time = '08:00';
   let symptom = '';
 
   let nameError = '';
