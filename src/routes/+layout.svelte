d133c3a131260a7150d3c607cb6ca8375aa4d2ac
LinhBQ <linhstartwork@gmail.com>
2024-09-30 12:22:27 +0700

    sds

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -401,7 +401,7 @@
             <span style="color: red;">{phoneError}</span>
           {/if}
         </td>
-        <td><Flatpickr class='date-picker' bind:value={dob} placeholder='Ngày sinh' allowInput/></td>
+        <td><Flatpickr class='date-picker' bind:value={dob} placeholder='Ngày sinh' altInput/></td>
   
       </tr>
       <tr>
